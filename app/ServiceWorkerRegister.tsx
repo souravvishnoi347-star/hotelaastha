ba4b5a3e51a68ce5c44b82ef243903ff2f35d3ac
souravvishnoi347-star <souravvishnoi347-star@users.noreply.github.com>
2026-08-05 13:14:04 +0530

    Add Service Worker for PWA compliance

---
+++ app/ServiceWorkerRegister.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => console.log("SW registered: ", registration))
+        .catch((err) => console.log("SW registration failed: ", err));
+    }
+  }, []);
+
+  return null;
+}
